d1ea9cf29de7c7b0b875dda7faa0e307a167c83f
AinhoaAlonso <ainhoaalonso88@gmail.com>
2024-03-14 19:59:15 +0100

    Checkpoint5

---
+++ ejercicios.py
@@ -0,0 +1,34 @@
+#Cree un bucle For de Python.
+
+num_list = range (1,11)
+
+for num in num_list:
+    print(num)
+
+#Cree una función de Python llamada suma que tome 3 argumentos y devuelva la suma de los 3.
+
+def suma(num1, num2, num3):
+    total = num1 + num2 + num3
+    return total
+print(suma(3,5,7))
+
+#Cree una función lambda con la misma funcionalidad que la función de suma que acaba de crear.
+
+suma = lambda num1, num2, num3: num1 + num2 + num3
+
+print(suma(3,5,7))
+
+"""Utilizando la siguiente lista y variable, determine si el valor de la variable coincide o no con un valor de la lista. *Sugerencia, si es necesario, utilice un bucle for in y el operador in.
+
+nombre = 'Enrique'
+
+lista_nombre = 'Jessica', 'Paul', 'George', 'Henry', 'Adán'"""
+
+lista_nombre = ['Jessica', 'Paul', 'George', 'Henry', 'Adán']
+
+for nombre in lista_nombre:
+    if nombre == 'Enrique':
+        print('El nombre es igual a Enrique')
+    else:
+        print('El nombre Enrique no está en la lista')
+
